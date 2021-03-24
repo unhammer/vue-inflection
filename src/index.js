49380c44d27c5031aec71e1de75161a3d994fc52
Ole Voldsæter <ole.voldsater@uib.no>
2021-03-24 09:55:48 +0100

    endring i import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import InflectionTable from './components/inflectionTable.vue'
+import inflectionTable from './components/inflectionTable.vue'
 
 export default {
     install(Vue, options) {
-        Vue.component('inflectionTable', InflectionTable)
+        Vue.component('inflectionTable', inflectionTable)
     }
 }
